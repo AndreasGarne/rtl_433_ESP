2de3a49b7398076c90b280a799849e2ed7b39d53
BadgerBoy81 <sir_hiss@hotmail.com>
2024-11-08 09:20:27 +0100

    change nexa doorbell timings

--- src/rtl_433/devices/nexa_doorbell.c
+++ src/rtl_433/devices/nexa_doorbell.c
@@ -10,10 +10,6 @@ static int nexa_doorbell_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     int device;
     uint8_t *b;
 
-    decoder_log(decoder, -2, __func__, "start");
-
-    decoder_log(decoder, -2, __func__, "get repeated row");
-
 //     r = bitbuffer_find_repeated_row(bitbuffer, bitbuffer->num_rows > 5 ? 5 : 3, 12); // 32
 
 //     decoder_logf(decoder, -2, __func__, "rows: %i", r);
@@ -32,21 +28,17 @@ static int nexa_doorbell_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     //     return DECODE_ABORT_EARLY;
     // }
 
-    decoder_log(decoder, -2, __func__, "get actual data");
-    decoder_logf(decoder, -2, __func__, "syncs before first row: %i", bitbuffer->syncs_before_row[0]);
-    decoder_logf(decoder, -2, __func__, "num rows: %i", bitbuffer->num_rows);
-        return DECODE_ABORT_EARLY;
-//     int i;
-//     for (i = 0; i < bitbuffer->num_rows; i++)
-//     {
-//         decoder_logf(decoder, -2, __func__, "row: %i", i);
-//         decoder_logf(decoder, -2, __func__, "row size", bitbuffer->bits_per_row[i]);
-//         decoder_logf(decoder, -2, __func__, "row: %i, num bits: %i", i, bitbuffer->bits_per_row[i]);
+    int i;
+    for (i = 0; i < bitbuffer->num_rows; i++)
+    {
+        decoder_logf(decoder, -2, __func__,"row: %i, data length: %i, hex: %x, leading sync bits: %i", i, bitbuffer->bits_per_row[i], bitbuffer->bb[i], bitbuffer->syncs_before_row[i]);
+    }
 
-//         decoder_log(decoder, -2, __func__,sprintf("row: %i, data length: %i", i, bitbuffer->bits_per_row[i]));
-//     }
+    decoder_logf(decoder, -2, __func__,"num_rows: %i, i: %i", bitbuffer->num_rows, i);
     // b = bitbuffer->bb[r];
 
+
+    return DECODE_ABORT_EARLY;
     /* clang-format off */
     data = data_make(
             "id", "", DATA_STRING, "NEXA_DOORBELL",
@@ -90,13 +82,20 @@ static char const *output_fields[] = {
 // note TX141W, TX145wsdth: m=OOK_PWM, s=256, l=500, r=1888, y=748
 r_device nexa_doorbell = {
         .name        = "NEXA door bell",
-        .modulation  = OOK_PULSE_PWM,
-        .short_width = 256,  // short pulse is 208 us + 417 us gap
-        .long_width  = 320,  // long pulse is 417 us + 208 us gap
-        .sync_width  = 0,  // sync pulse is 833 us + 833 us gap
+        .modulation  = OOK_PULSE_PPM,
+        .short_width = 254,  // 1:1
+        .long_width  = 1270, // 1:5
+        .sync_width  = 2540, // 1:10
+        .tolerance   = 100,
+        .gap_limit   = 1500,
+        .reset_limit = 10688,
+        // .modulation  = OOK_PULSE_PWM,
+        // .short_width = 256,  // short pulse is 208 us + 417 us gap
+        // .long_width  = 320,  // long pulse is 417 us + 208 us gap
+        // .sync_width  = 0,  // sync pulse is 833 us + 833 us gap
 //        .tolerance   = 200,
-        .gap_limit   = 260,  // long gap (with short pulse) is ~417 us, sync gap is ~833 us
-        .reset_limit = 10688, // maximum gap is 1250 us (long gap + longer sync gap on last repeat)
+        // .gap_limit   = 260,  // long gap (with short pulse) is ~417 us, sync gap is ~833 us
+        // .reset_limit = 10688, // maximum gap is 1250 us (long gap + longer sync gap on last repeat)
         .decode_fn   = &nexa_doorbell_decode,
         .fields      = output_fields,
 };
--- src/rtl_433/devices/texa_fan_remote.c
+++ src/rtl_433/devices/texa_fan_remote.c
@@ -32,6 +32,7 @@ static int texa_fan_remote_decode(r_device *decoder, bitbuffer_t *bitbuffer)
         return DECODE_ABORT_EARLY;
     }
 
+        return DECODE_ABORT_EARLY;
     decoder_log(decoder, -2, __func__, "get actual data");
 
     b = bitbuffer->bb[r];
